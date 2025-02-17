ce96917abb52452f74a20c3d8b784f8468e030bc
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-02-17 06:46:30 +0200

    Some edits

---
+++ app/Http/Middleware/CheckTrialPeriod.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Carbon\Carbon;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckTrialPeriod
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     */
+
+
+    public function handle($request, Closure $next)
+    {
+        if (Auth::guard('web')->check()) {
+            $user = Auth::user();
+
+            if (!$user->status) {
+                Auth::logout();
+                return redirect('/login')->withErrors(['message' => 'Your account is not active. Please contact the administrator.']);
+            }
+
+            $trialPeriod = Carbon::parse($user->created_at)->addDays(14);
+            $today = Carbon::now();
+
+            if ($today->greaterThan($trialPeriod) && !$user->created_at) {
+                Auth::logout();
+                return redirect('/login')->withErrors(['message' => 'Your trial period has expired and your account is not activated by admin.']);
+            }
+        }
+
+        return $next($request);
+    }
+
+}
