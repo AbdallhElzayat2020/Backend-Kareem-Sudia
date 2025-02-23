ea0b3493be00bd2100584646351d765683e889c8
ahmdsadik <ahmed.sadik.dev@gmail.com>
2025-02-23 23:47:59 +0200

    Add admin routing

--- routes/admin.php
+++ routes/admin.php
@@ -3,16 +3,14 @@
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin', function () {
+Route::get('/', function () {
     return 'Admin Route is Working!';
 });
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::get('/login', [AdminController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [AdminController::class, 'login']);
-});
+Route::get('/login', [AdminController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AdminController::class, 'login']);
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth:admin', 'admin']], function () {
+Route::group(['middleware' => ['auth:admin', 'admin']], function () {
     Route::get('/dashboard/home', [AdminController::class, 'index'])->name('dashboard');
     Route::post('/logout', [AdminController::class, 'logout'])->name('logout');
 });
